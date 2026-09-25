066a6722d430d47a3890ee44c574dee98b35c52f
mariano-10r-ay2526 <mariano.dl@obmontessori.edu.ph>
2026-09-25 15:26:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ def create_order(e):
     prod3 = document.getElementById("food3")
     prod4 = document.getElementById("food4")
 
-    # Calculate the subtotal
+# Calculate the subtotal
     subtotal = float(prod1.value) * prod1.checked + float(prod2.value) * prod2.checked + float(prod3.value) * prod3.checked + float(prod4.value) * prod4.checked
     display(f'The subtotal is ₱{subtotal:.2f}', target="show")
 
-    # Calculate the VAT
+# Calculate the VAT
     VAT = subtotal * 0.12
     display(f'The VAT is ₱{VAT:.2f}', target="show")
 
-    # Calculate the total
+# Calculate the total
     total = subtotal + VAT
     display(f'The total amount is ₱{total:.2f}', target="show")
