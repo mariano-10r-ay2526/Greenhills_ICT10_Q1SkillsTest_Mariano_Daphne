81538172977046e70a1bd419d8298d8df0ff2bec
mariano-10r-ay2526 <mariano.dl@obmontessori.edu.ph>
2026-09-25 15:24:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
 # Arithmetic Operators
 from pyscript import display, document
 
-
 def create_order(e):
     prod1 = document.getElementById("food1")
     prod2 = document.getElementById("food2")
     prod3 = document.getElementById("food3")
     prod4 = document.getElementById("food4")
 
-    # Calculate subtotal by multiplying value by checked status (1 or 0)
-    subtotal = float(prod1.value) * prod1.checked
-    subtotal = float(prod2.value) * prod2.checked
-    subtotal = float(prod3.value) * prod3.checked
-    subtotal = float(prod4.value) * prod4.checked
+    # Calculate the subtotal
+    subtotal = (float(prod1.value) * prod1.checked) + \
+               (float(prod2.value) * prod2.checked) + \
+               (float(prod3.value) * prod3.checked) + \
+               (float(prod4.value) * prod4.checked)
     display(subtotal, target="show")
 
-    # Calculate VAT by getting 12% of the subtotal
+    # Calculate the VAT
     VAT = subtotal * 0.12
     display(VAT, target="show")
 
-    # Calculate total by adding both values
+    # Calculate the total
     total = subtotal + VAT
     display(total, target="show")
