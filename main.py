e078a5aa2b51ec526216b1412137a4ddb522d2eb
mariano-10r-ay2526 <mariano.dl@obmontessori.edu.ph>
2026-09-25 15:25:35 +0800

    Refactor create_order function for better output

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-# Arithmetic Operators
 from pyscript import display, document
+
+# Arithmetic Operators
 
 def create_order(e):
     prod1 = document.getElementById("food1")
@@ -8,16 +9,13 @@ def create_order(e):
     prod4 = document.getElementById("food4")
 
     # Calculate the subtotal
-    subtotal = (float(prod1.value) * prod1.checked) + \
-               (float(prod2.value) * prod2.checked) + \
-               (float(prod3.value) * prod3.checked) + \
-               (float(prod4.value) * prod4.checked)
-    display(subtotal, target="show")
+    subtotal = float(prod1.value) * prod1.checked + float(prod2.value) * prod2.checked + float(prod3.value) * prod3.checked + float(prod4.value) * prod4.checked
+    display(f'The subtotal is ₱{subtotal:.2f}', target="show")
 
     # Calculate the VAT
     VAT = subtotal * 0.12
-    display(VAT, target="show")
+    display(f'The VAT is ₱{VAT:.2f}', target="show")
 
     # Calculate the total
     total = subtotal + VAT
-    display(total, target="show")
+    display(f'The total amount is ₱{total:.2f}', target="show")
